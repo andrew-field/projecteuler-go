2fccfd31fafdd970a3c3343b3ae81f1615cfc955
Andrew Field <af02@hotmail.co.uk>
2017-04-21 21:47:10 +0300

    Add files via upload

---
+++ even_fibonacci_numbers.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main () {
+
+num1 := 1
+num2 := 1
+buffer := 0
+total := 0
+
+for num2<4000000 {
+    if num2%2 == 0 {
+        total += num2        
+    }
+    buffer = num2
+    num2 += num1
+    num1 = buffer
+}
+
+fmt.Println(total)
+
+}
---
+++ multiplesof3and5.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main () {
+
+total := 0
+
+for i:=1;i<=999;i++ {
+    if i%3 == 0 || i%5 == 0 {
+        total += i
+    }
+}
+
+fmt.Println(total)
+
+}
