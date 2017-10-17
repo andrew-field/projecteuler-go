1f3ee12a22bd4cc9e86bdbcd6d1dbf88aa18b363
andrew-field <andrewpcfield@gmail.com>
2017-10-17 18:04:05 +0100

    15

---
+++ 15latticepaths.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "math/big"
+
+// This program is just used to calculate the binomial coefficient. Thsé problem is just picking k choices from n scenarios, where order does not matter.
+// Hence, binomal coefficient. The problem can be thought of by making 40 steps/paths with only 20 independent choices.
+
+func main() {
+	fmt.Println("Answer:", big.NewInt(0).Binomial(40, 20).Int64())
+}
