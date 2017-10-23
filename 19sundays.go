675e25c3675317bc8a1d340df07689ecd2a94d06
andrew-field <andrewpcfield@gmail.com>
2017-10-23 16:07:52 +0100

    19

---
+++ 19sundays.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	count := 0
+	date := time.Date(1901, 01, 06, 1, 1, 1, 000000000, time.UTC)
+	for date.Year() != 2001 {
+		date = date.AddDate(0, 0, 7)
+		if date.Day() == 1 {
+			count++
+		}
+	}
+	fmt.Println("Count", count)
+}
