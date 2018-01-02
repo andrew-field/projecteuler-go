e92bbe8e0916f09dbcfaa44cf32b870b96e5ca87
andrew-field <andrewpcfield@gmail.com>
2018-01-02 12:22:08 +0000

    pdate

--- 21ammicablenumbers.go
+++ 21ammicablenumbers.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+func power(x, y int) int {
+	for num := x; y != 1; y-- {
+		x *= num
+	}
+	return x
+}
 
 func main() {
 	// Slice for primes.
-	primes := make([]int, 96)
+	primes := make([]int, 4998)
 
 	// Slice for numbers.
 	nums := make([]int, 10001)
@@ -17,7 +22,7 @@ func main() {
 	// Generate prime numbers through a euclidean sieve.
 	for ind, val := range primes {
 		if val != 1 {
-			for j := ind + val; j < 96; j += val {
+			for j := ind + val; j < len(primes); j += val {
 				if primes[j] != 1 {
 					primes[j] = 1
 				}
@@ -26,14 +31,17 @@ func main() {
 		}
 	}
 
-	// Finish the prime array slices (Get rid of the ones).
-	for i := 0; primes[i] != 97; i++ {
+	// Finish the prime slice (Get rid of the ones).
+	for i := 0; primes[i] != 4999; i++ {
 		if primes[i] == 1 {
 			primes = append(primes[:i], primes[i+1:]...)
 			i--
 		}
 	}
 
+	// Go through the numbers, find all the prime factors of each number, calculate all the factors of the number,
+	// store the sum of all the factors at the location in the number array. ind 1 == number 1, the value stored there
+	// is d(1).
 	for ind := range nums {
 		num := ind + 1
 		factors := make([]int, 0)
@@ -46,9 +54,10 @@ func main() {
 				break
 			}
 		}
-		total := 0
+		if num != 1 {
+			factors = append(factors, 0)
+		}
+		// total := 0
 
 	}
-
-	fmt.Println(primes)
 }
