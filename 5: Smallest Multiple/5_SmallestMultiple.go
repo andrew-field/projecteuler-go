13d9f1f9bf92a7668d2988a63e7acf965f72732f
Andrew Field <andrewpcfield@gmail.com>
2018-09-13 16:20:27 +0300

    Challenge 5 and update number theory library.

--- 5: Smallest Multiple/5_SmallestMultiple.go
+++ 5: Smallest Multiple/5_SmallestMultiple.go
@@ -1,48 +1,47 @@
-// Quicker by hand (lcm).
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/andrew-field/testing_go/numbertheory"
+)
 
 func main() {
 
-	// Primes below 20.
-	primes := []int{2, 3, 5, 7, 11, 13, 17, 19}
+	// // Old method.
+	// // Primes below 20.
+	// primes := []int{2, 3, 5, 7, 11, 13, 17, 19}
 
-	// Numbers below 20, not 1.
-	num := make([]int, 19)
-	for ind := range num {
-		num[ind] = ind + 2
+	// Old and current method.
+	// Numbers to 20, not 1.
+	numbers := make([]uint, 19)
+	for ind := range numbers {
+		numbers[ind] = uint(ind) + 2
 	}
 
-	total := 1
+	fmt.Println("Total:", numbertheory.LowestCommonMultiple(numbers...))
 
-	// lcm by raising each prime number to the maximum number of times
-	// that prime appears in any of the nums and then multiplying each of
-	// these results.
-	for _, prime := range primes {
-		count := 0
-		for _, val := range num {
-			temp := 0
-			for val%prime == 0 {
-				temp++
-				val /= prime
-			}
-			if temp > count {
-				count = temp
-			}
-		}
-		// mult becomes prime rasied to the power count.
-		mult := 1
-		for count > 0 {
-			mult *= prime
-			count--
-		}
+	// // Old Method.
+	// var total float64 = 1
 
-		// Multiply to total.
-		total *= mult
-	}
+	// // lcm by raising each prime number to the maximum number of times
+	// // that prime number appears in any of the numbers and then multiplying each of
+	// // these results.
+	// for _, prime := range primes {
+	// 	count := 0
+	// 	for _, val := range num {
+	// 		temp := 0
+	// 		for ; val%prime == 0; temp++ {
+	// 			val /= prime
+	// 		}
+	// 		if temp > count {
+	// 			count = temp
+	// 		}
+	// 	}
 
-	fmt.Println("Total:", total)
+	// 	// Multiply to total.
+	// 	total *= math.Pow(float64(prime), float64(count))
+	// }
 
+	// fmt.Println("Total:", int(total))
 }
---
+++ numbertheory/lowestCommonMultiple.go
@@ -0,0 +1,48 @@
+package numbertheory
+
+// LowestCommonMultiple returns the lowest common multiple (lcm) of a group of numbers.
+func LowestCommonMultiple(numbers ...uint) uint {
+
+	// Slice of slices containing the prime factorisation of each number.
+	primeFactorisations := make([][]uint, 0)
+
+	// Fill slices
+	for _, val := range numbers {
+		newFactorisation := make([]uint, 0)
+		factorChannel := make(chan uint, 100)
+		go GetPrimeFactorisation(factorChannel, val)
+		for val := range factorChannel {
+			newFactorisation = append(newFactorisation, val)
+		}
+		primeFactorisations = append(primeFactorisations, newFactorisation)
+	}
+
+	// For each prime number, any duplicates are removed from the other factorisations (once).
+	// This is so each prime number appears the correct number of times for the lcm calculation.
+	// Another way to think about it is that each prime number appears exactly the maximum number
+	// of times it appears in any individual factorisation.
+	// See lcm calculation.
+	for ind, val := range primeFactorisations[:len(primeFactorisations)-1] {
+		for _, prime := range val {
+			for ind2, val2 := range primeFactorisations[ind+1:] {
+				for ind3, val3 := range val2 {
+					if val3 == prime {
+						primeFactorisations[ind+1+ind2][len(val2)-1], primeFactorisations[ind+1+ind2][ind3] = primeFactorisations[ind+1+ind2][ind3], primeFactorisations[ind+1+ind2][len(val2)-1]
+						primeFactorisations[ind+1+ind2] = primeFactorisations[ind+1+ind2][:len(val2)-1]
+						break
+					}
+				}
+			}
+		}
+	}
+
+	// Multiply the remaining primes together.
+	var product uint = 1
+	for _, val := range primeFactorisations {
+		for _, prime := range val {
+			product *= prime
+		}
+	}
+
+	return product
+}
