06b8e2e794c57a977c67f0f85237b768796b8229
Andrew Field <andrewpcfield@gmail.com>
2018-09-14 17:23:05 +0300

    Challenge 9.

--- 9: Special Pythagorean Triplet/9_SpecialPythagoreanTriplet.go
+++ 9: Special Pythagorean Triplet/9_SpecialPythagoreanTriplet.go
@@ -3,28 +3,20 @@ package main
 import "fmt"
 import "math"
 
-const val = 1000
-
 func main() {
 
 	var a float64 = 1
-	var b float64 = 1
 
 	// Cycle through possible a's and b's.
-	for a < 1000 {
-		for b < 1000 {
+	for ; a < 1000; a++ {
+		for b := a; b < 1000; b++ {
 			// This must be true with the two simultaneous equations.
-			if c := math.Hypot(a, b); a+b+c == val {
+			if c := math.Hypot(a, b); a+b+c == 1000 {
 				// As there is only one triplet for which this works, and a and b are 'fine', I don't need to make further checks on c.
-				//I.e. it must be the integer in quesion.
-				fmt.Println("Product:", int64(a*b*c))
-				a = 999
-				break
-			} else {
-				b++
+				// It must be the integer in quesion.
+				fmt.Println("Product:", int(a*b*c))
+				return
 			}
 		}
-		a++
-		b = a
 	}
 }
