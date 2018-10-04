7abb03799c38ea92ae466f9717c414d50c8108df
Andrew Field <andrewpcfield@gmail.com>
2018-10-04 19:49:08 +0100

    Added testing file.

---
+++ Miscellaneous/TestingFile.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/andrew-field/testing_go/numbertheory"
+)
+
+func main() {
+	for i := 2; i < 10; i++ {
+		primeFactorChannel := make(chan uint, 100)
+		numbertheory.GetPrimeFactorisation(primeFactorChannel, uint(i))
+		fmt.Println("i:", i)
+		for val := range primeFactorChannel {
+			fmt.Println(val)
+		}
+	}
+}
