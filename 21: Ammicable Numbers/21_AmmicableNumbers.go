c5283d2677438d97b0a74ea603d7d878e1433696
Andrew Field <andrewpcfield@gmail.com>
2018-10-27 13:32:03 +0300

    Challenge 21. Still need to look at the divisors functions to see if there is a better way.

--- 21: Ammicable Numbers/21_AmmicableNumbers.go
+++ 21: Ammicable Numbers/21_AmmicableNumbers.go
@@ -1,63 +1,36 @@
 package main
 
-func power(x, y int) int {
-	for num := x; y != 1; y-- {
-		x *= num
-	}
-	return x
-}
+import (
+	"fmt"
+
+	"github.com/andrew-field/testing_go/numbertheory"
+)
 
 func main() {
-	// Slice for primes.
-	primes := make([]int, 4998)
+	sumOfProperDivisors := make([]uint, 10000)
 
-	// Slice for numbers.
-	nums := make([]int, 10001)
+	var index uint = 2
+	for ; index < 10000; index++ {
+		divisorChannel := numbertheory.GetDivisorsOfANumber(index)
 
-	// Make slice ready for prime numbers.
-	for ind := range primes {
-		primes[ind] = ind + 2
+		// The divisors includes the number itself but the challenge does not.
+		sum := -index
+		for val := range divisorChannel {
+			sum += val
+		}
+
+		sumOfProperDivisors[index] = sum
 	}
 
-	// Generate prime numbers through a euclidean sieve.
-	for ind, val := range primes {
-		if val != 1 {
-			for j := ind + val; j < len(primes); j += val {
-				if primes[j] != 1 {
-					primes[j] = 1
-				}
-			}
-
+	var sumOfAmicableNumbers uint
+	for ind, val := range sumOfProperDivisors {
+		if val != 1 && val != 0 && uint(ind) != val && val < 10000 && sumOfProperDivisors[val] == uint(ind) {
+			sumOfAmicableNumbers += uint(ind) + val
+			fmt.Println(ind, val)
+			sumOfProperDivisors[val] = 1
+			sumOfProperDivisors[ind] = 1
 		}
 	}
 
-	// Finish the prime slice (Get rid of the ones).
-	for i := 0; primes[i] != 4999; i++ {
-		if primes[i] == 1 {
-			primes = append(primes[:i], primes[i+1:]...)
-			i--
-		}
-	}
-
-	// Go through the numbers, find all the prime factors of each number, calculate all the factors of the number,
-	// store the sum of all the factors at the location in the number array. ind 1 == number 1, the value stored there
-	// is d(1).
-	for ind := range nums {
-		num := ind + 1
-		factors := make([]int, 0)
-		for _, val := range primes {
-			for num%val == 0 {
-				factors = append(factors, val)
-				num /= val
-			}
-			if num == 1 {
-				break
-			}
-		}
-		if num != 1 {
-			factors = append(factors, 0)
-		}
-		// total := 0
-
-	}
+	fmt.Println("Answer:", sumOfAmicableNumbers)
 }
