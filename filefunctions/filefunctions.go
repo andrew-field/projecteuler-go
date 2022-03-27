719900f1a38126dcb20ca18376bea84d485367fd
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2022-03-27 19:24:09 +0300

    Update CloseFile

--- filefunctions/filefunctions.go
+++ filefunctions/filefunctions.go
@@ -1,7 +1,6 @@
 package filefunctions
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -25,8 +24,5 @@ func OpenFile(path string) *os.File {
 // CloseFile closes a file pointer.
 func CloseFile(f *os.File) {
 	err := f.Close()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
+	Check(err)
 }
