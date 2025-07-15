ed96b816fa2d6b47bc015cd1353507e8541caaae
andrew-field <27866671+andrew-field@users.noreply.github.com>
2025-07-15 15:08:04 +0300

    feat: add checks for zeroes to problem 8 (#75)

--- euler1/8_largest_product_in_a_series.go
+++ euler1/8_largest_product_in_a_series.go
@@ -29,6 +29,7 @@ package euler1
 
 import (
 	"math/big"
+	"slices"
 
 	"github.com/andrew-field/maths/v2"
 )
@@ -46,11 +47,13 @@ func largestProductInASeries() int {
 	var check int
 	var maxProduct int
 
-	// Not the most efficient as 13 calculations are wasted for every zero but it is simple enough.
-	for j := range 987 {
-		if check = digits[j] * digits[j+1] * digits[j+2] * digits[j+3] * digits[j+4] * digits[j+5] * digits[j+6] * digits[j+7] * digits[j+8] * digits[j+9] * digits[j+10] * digits[j+11] * digits[j+12]; check > maxProduct {
-			maxProduct = check
+	for j := 0; j < 988; j++ {
+		check = digits[j] * digits[j+1] * digits[j+2] * digits[j+3] * digits[j+4] * digits[j+5] * digits[j+6] * digits[j+7] * digits[j+8] * digits[j+9] * digits[j+10] * digits[j+11] * digits[j+12]
+		if check == 0 { // Skip to the next set of digits after the zero.
+			j += slices.Index(digits[j:], 0)
+			continue
 		}
+		maxProduct = max(maxProduct, check)
 	}
 
 	return maxProduct
