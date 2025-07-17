e6562387ef6cad7d224443255144a0e8d0d91f8d
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2025-07-17 19:58:08 +0300

    feat: complete challenge

--- euler2/23_non_abundant_sums.go
+++ euler2/23_non_abundant_sums.go
@@ -10,6 +10,44 @@ Find the sum of all the positive integers which cannot be written as the sum of 
 
 package euler2
 
+import (
+	"github.com/andrew-field/maths/v2"
+)
+
 func nonAbundantSums() int {
-	return 1
+	// We know 12 is the first abundant number.
+	abundantNumbers := []int{12}
+
+	// 28122 is the largest number we need to check to be the sum of two abundant numbers.
+	// As 12 is the smallest abundant number, the largest number we need to check for being abundant is 28110.
+	for i := 13; i < 28111; i++ {
+		sumOfDivisors, err := maths.SumOfDivisors(i)
+		if err != nil {
+			panic(err)
+		}
+		if sumOfDivisors-i > i {
+			abundantNumbers = append(abundantNumbers, i)
+		}
+	}
+
+	// We will calculate the final sum by subtracting the sum of all positive integers which can be written as the sum of two abundant numbers up to 28123 from the sum of all positive integers up to 28123.
+	// Sum of all integers from 1 to 28122, using the formula n(n+1)/2.
+	totalSum := 28122 * 28123 / 2
+
+	abundantSums := map[int]struct{}{}
+
+	// Calculate all the possible sums of two abundant numbers, less than 28123 and subtract them from the total sum.
+	for i := 0; i < len(abundantNumbers); i++ {
+		for j := i; j < len(abundantNumbers); j++ {
+			if abundantNumbers[i]+abundantNumbers[j] < 28123 {
+				sum := abundantNumbers[i] + abundantNumbers[j]
+				if _, ok := abundantSums[sum]; !ok {
+					abundantSums[sum] = struct{}{}
+					totalSum -= sum
+				}
+			}
+		}
+	}
+
+	return totalSum
 }
