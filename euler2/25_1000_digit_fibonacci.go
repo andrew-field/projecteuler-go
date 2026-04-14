3112d1bb91e8d25d9d9b801444fbb45c47925344
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2026-04-15 00:10:15 +0300

    Revert "fix: test"
    This reverts commit bbb20e73cdc84e1d23c27f266d179c34cabf22ce.

--- euler2/25_1000_digit_fibonacci.go
+++ euler2/25_1000_digit_fibonacci.go
@@ -27,7 +27,6 @@ What is the index of the first term in the Fibonacci sequence to contain 1000 di
 package euler2
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/andrew-field/maths/v2"
@@ -42,13 +41,6 @@ func digitFibonacci(n int) int {
 		fib.Set(num1)
 		num1.Set(num2)
 		num2.Add(fib, num2)
-	}
-
-	if true {
-		fmt.Println("test")
-		return i
-	} else {
-		fmt.Println("fdsf")
 	}
 
 	return i
