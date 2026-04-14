7cc1eb06dd51186589799828aff3a41d8b9e1b33
andrew-field <27866671+andrew-field@users.noreply.github.com>
2026-04-15 00:18:17 +0300

    fix: exclude text file from linting (#111)

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package projecteuler is a "container" module.
+// The files are organized in subdirectories (euler1, euler2, ...).
+// This file is mainly here so the super linter (and golangci-lint under the hood) does not throw an error and can lint all the files correctly.
+package projecteuler
