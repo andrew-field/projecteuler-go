46ebbac5ba6ade6e72ab431d4dbf2103e767ebd4
andrew-field <27866671+andrew-field@users.noreply.github.com>
2026-07-05 12:04:24 +0000

    Update 67_maximum_path_sum_two.go
    Signed-off-by: andrew-field <27866671+andrew-field@users.noreply.github.com>

--- euler4/67_maximum_path_sum_two.go
+++ euler4/67_maximum_path_sum_two.go
@@ -1,12 +1,12 @@
 /*
-By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 26.
 
 3
-7 4
-2 4 6
+9 4
+2 5 6
 8 5 9 3
 
-That is, 3 + 7 + 4 + 9 = 23.
+That is, 3 + 9 + 5 + 9 = 26.
 
 Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 
